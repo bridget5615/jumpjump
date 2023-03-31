66adcc4047969fe73792ed335ef8d375b374c398
southlatitude <128211831+southlatitude@users.noreply.github.com>
2023-04-01 01:01:12 +0800

    quit play mode

--- JumpJump!/Assets/Script/Quit.cs
+++ JumpJump!/Assets/Script/Quit.cs
@@ -4,19 +4,16 @@ using UnityEngine;
 
 public class Quit : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            QuitGame();
+        }
     }
-    public void Quitgame()
+    public void QuitGame()
     {
+        UnityEditor.EditorApplication.isPlaying = false;
         Application.Quit();
     }
 }
