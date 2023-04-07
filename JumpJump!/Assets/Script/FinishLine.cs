90e7503255857dbf2816d45715a5cfd93d32293b
southlatitude <128211831+southlatitude@users.noreply.github.com>
2023-04-07 13:00:34 +0800

    change leg and score bug solved

--- JumpJump!/Assets/Script/FinishLine.cs
+++ JumpJump!/Assets/Script/FinishLine.cs
@@ -2,13 +2,33 @@ using UnityEngine;
 
 public class FinishLine : MonoBehaviour
 {
-    public GameObject levelCompleteScreen;
+    public GameObject levelCompleteScreenNext;
+    public GameObject levelCompleteScreenchangeleg;
+    public static int levelcompletetimes;
+
+
+
     private void OnTriggerEnter2D(Collider2D collideFinish)
     {
         if (collideFinish.gameObject.CompareTag("Finish"))
         {
-            levelCompleteScreen.SetActive(true);
-            Time.timeScale = 0;
+            levelcompletetimes = levelcompletetimes + 1;
+            if (levelcompletetimes%2 == 0)
+            {
+
+                levelCompleteScreenNext.SetActive(true);
+                Time.timeScale = 0;
+
+            }
+
+            else
+            {
+
+                levelCompleteScreenchangeleg.SetActive(true);
+                Time.timeScale = 0;
+                
+            }
+            
         }
 
     }
