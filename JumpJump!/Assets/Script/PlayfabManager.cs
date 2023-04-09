7f76101683892fab06e67f4a4b5f7b60bd79d720
southlatitude <128211831+southlatitude@users.noreply.github.com>
2023-04-09 17:15:50 +0800

    leaderboard added

--- JumpJump!/Assets/Script/PlayfabManager.cs
+++ JumpJump!/Assets/Script/PlayfabManager.cs
@@ -1,15 +1,28 @@
 using System.Collections.Generic;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 using PlayFab;
 using PlayFab.ClientModels;
 using TMPro; 
-
-// susing Newtonsoft.Json;
-using UnityEngine.UI;
+// using Newtonsoft.Json;
 
 public class PlayfabManager : MonoBehaviour
 {
+
+    [Header("Leaderboard")]
+    public GameObject rowPrefab;
+    public Transform rowsParent;
+
+    [Header("Windows")]
+    public GameObject nameWindow;
+    public GameObject leaderboardWindow;
+
+
+    [Header("Display name window")]
+    public GameObject nameError;
+    public TMP_InputField nameinput;
+
     [Header("UI")]
     public TextMeshProUGUI messageText;
     
@@ -63,6 +76,14 @@ public class PlayfabManager : MonoBehaviour
     void OnLoginSuccess(LoginResult result)
     {
         messageText.text = "Logged in successfully!";
+        string name = null;
+        if(result.InfoResultPayload.PlayerProfile != null)
+            name = result.InfoResultPayload.PlayerProfile.DisplayName;
+        
+        if (name == null)
+            nameWindow.SetActive(true);
+        else
+            leaderboardWindow.SetActive(true);
     }
 
 
@@ -103,7 +124,11 @@ void onPasswordReset(SendAccountRecoveryEmailResult result)
     {
         var request = new LoginWithCustomIDRequest { 
             CustomId = SystemInfo.deviceUniqueIdentifier, 
-            CreateAccount = true 
+            CreateAccount = true,
+            InfoRequestParameters = new GetPlayerCombinedInfoRequestParams
+            {
+                GetPlayerProfile = true
+            }
         };
         PlayFabClientAPI.LoginWithCustomID(request, OnSuccess, OnError);
     }
@@ -183,14 +208,42 @@ void onPasswordReset(SendAccountRecoveryEmailResult result)
 
     void OnLeaderboardGet(GetLeaderboardResult result)
     {
-        foreach (var entry in result.Leaderboard)
+        foreach (Transform item in rowsParent)
         {
-            UnityEngine.Debug.Log(entry.Position + " " + entry.PlayFabId + " " + entry.StatValue);
+            Destroy(item.gameObject);
+        }
+
+        foreach (var item in result.Leaderboard)
+        {
+            GameObject newGo = Instantiate(rowPrefab,rowsParent);
+            Text[] texts = newGo.GetComponentsInChildren<Text>();
+            texts[0].text = (item.Position +1).ToString();
+            texts[1].text = item.DisplayName;
+            texts[2].text = item.StatValue.ToString();
+
+            Debug.Log(string.Format("PLACE:{0}| ID:{1}| VALUE:{2}",
+            item.Position, item.PlayFabId, item.StatValue));
+
         }
     }
     
     void OnLeaderboardUpdate(UpdatePlayerStatisticsResult result)
     {
         UnityEngine.Debug.Log("Leaderboard updated!");
+    }
+
+    public void SubmitNameButton()
+    {
+        var request = new UpdateUserTitleDisplayNameRequest
+        {
+            DisplayName = nameinput.text,
+        };
+        PlayFabClientAPI.UpdateUserTitleDisplayName(request, OnDisplayNameUpdate, OnError);
+    }
+
+    void OnDisplayNameUpdate(UpdateUserTitleDisplayNameResult result)
+    {
+        Debug.Log("Updated display name!");
+        leaderboardWindow.SetActive(true);
     }
 }
