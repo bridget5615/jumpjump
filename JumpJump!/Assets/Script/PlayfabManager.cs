28a4d4d39e228d1c0b2572c22d693635e7fe6720
bridget5615 <1005615@mymail.sutd.edu.sg>
2023-04-09 18:22:18 +0800

    slight adjustments

--- JumpJump!/Assets/Script/PlayfabManager.cs
+++ JumpJump!/Assets/Script/PlayfabManager.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Collections.Generic;
 using System.Collections;
 using UnityEngine;
 using PlayFab;
 using PlayFab.ClientModels;
+using PlayFab.Internal;
 using TMPro; 
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
@@ -12,43 +14,65 @@ public class PlayfabManager : MonoBehaviour
     [Header("UI")]
     public TextMeshProUGUI messageText;
     public TMP_InputField usernameInput;
+    
+
+    [Header("PlayFab")]
+    [SerializeField] private string titleId = "EBE9E"; // Replace with your title id
 
     private string displayName;
 
-    void Start()
+    void Awake()
     {
-        UnityEngine.Debug.Log("PlayfabManager Start called");
-
-        messageText.text = "Please key in your username to play :)";
+        PlayFabSettings.staticSettings.TitleId = titleId;
     }
 
     public void Login()
     {
-        var request = new LoginWithCustomIDRequest { 
-            CustomId = SystemInfo.deviceUniqueIdentifier, 
-            CreateAccount = true 
-        };
+        // Check if the username input field is empty
+        if (string.IsNullOrEmpty(usernameInput.text))
+        {
+            DisplayErrorMessage("Please enter a username :)");
+            return;
+        }
+
+        // Call the PlayFab login API with the device ID as the custom ID
+        string customId = SystemInfo.deviceUniqueIdentifier;
+        var request = new LoginWithCustomIDRequest { CustomId = customId, CreateAccount = true };
         PlayFabClientAPI.LoginWithCustomID(request, OnLoginSuccess, OnLoginError);
+    }
+
+
+
+    IEnumerator DelayedLoginSuccess(LoginResult result)
+    {
+        yield return new WaitForSeconds(1f);
+        OnLoginSuccess(result);
     }
 
     void OnLoginSuccess(LoginResult result)
     {
         messageText.text = "Login successful!";
-        UnityEngine.Debug.Log("Successful login/account created!");
+        Debug.Log("Successful login/account created!");
 
         // Update the user's display name in PlayFab
         UpdateDisplayName();
-        SceneManager.LoadScene("MainMenu"); 
+
+        StartCoroutine(DelayedLoadMainMenu());
+    }
+
+    IEnumerator DelayedLoadMainMenu()
+    {
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene("MainMenu");
     }
 
     void OnLoginError(PlayFabError error)
     {
         string errorMessage = error.GenerateErrorReport();
         DisplayErrorMessage(errorMessage);
-        UnityEngine.Debug.Log("Error while logging in/creating a new account");
-        UnityEngine.Debug.Log(errorMessage);
+        Debug.Log("Error while logging in/creating a new account");
+        Debug.Log(errorMessage);
     }
-
 
     void UpdateDisplayName()
     {
@@ -64,15 +88,16 @@ public class PlayfabManager : MonoBehaviour
 
     void OnDisplayNameUpdateSuccess(UpdateUserTitleDisplayNameResult result)
     {
-        UnityEngine.Debug.Log("Display name updated successfully!");
+        Debug.Log("Display name updated successfully!");
     }
 
     void OnDisplayNameUpdateError(PlayFabError error)
     {
         string errorMessage = error.GenerateErrorReport();
         DisplayErrorMessage(errorMessage);
-        UnityEngine.Debug.LogError("Failed to update display name: " + errorMessage);
+        Debug.LogError("Failed to update display name: " + errorMessage);
     }
+
     void DisplayErrorMessage(string message)
     {
         messageText.text = message;
@@ -111,7 +136,6 @@ public class PlayfabManager : MonoBehaviour
         yield return new WaitForSeconds(1f);
     }
 
-
     public void SendLeaderboardDelayedButton(int score)
     {
         StartCoroutine(SendLeaderboardWithDelay(score, 1f));
@@ -120,7 +144,34 @@ public class PlayfabManager : MonoBehaviour
     public IEnumerator SendLeaderboardWithDelay(int score, float delay)
     {
         yield return new WaitForSeconds(delay);
-        SendLeaderboardCoroutine(score);
+        var request = new UpdatePlayerStatisticsRequest
+        {
+            Statistics = new List<StatisticUpdate>
+            {
+                new StatisticUpdate
+                {
+                    StatisticName = "Platform Score",
+                    Value = score
+                }
+            }
+        };
+
+        PlayFabClientAPI.UpdatePlayerStatistics(request, OnLeaderboardUpdate, OnLeaderboardUpdateError);
+
+        // Wait for a short delay after the PlayFab API call
+        yield return new WaitForSeconds(1f);
+    }
+
+    void OnLeaderboardUpdate(UpdatePlayerStatisticsResult result)
+    {
+        UnityEngine.Debug.Log("Leaderboard updated successfully!");
+    }
+
+    void OnLeaderboardUpdateError(PlayFabError error)
+    {
+        string errorMessage = error.GenerateErrorReport();
+        DisplayErrorMessage(errorMessage);
+        UnityEngine.Debug.LogError("Failed to update leaderboard: " + errorMessage);
     }
 
     public void GetLeaderboard()
@@ -142,9 +193,9 @@ public class PlayfabManager : MonoBehaviour
         }
     }
 
-    void OnLeaderboardUpdate(UpdatePlayerStatisticsResult result)
+    void OnLeaderboardUpdateDelayed(UpdatePlayerStatisticsResult result)
     {
-        UnityEngine.Debug.Log("Leaderboard updated!");
+        UnityEngine.Debug.Log("Leaderboard updated with delay!");
     }
    
 }
